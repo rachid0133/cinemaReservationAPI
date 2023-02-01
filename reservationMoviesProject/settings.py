faaed4cae07030a45c3b81bba6e07a7665b1af61
rachid chakor <chakor.rachid.1@gmail.com>
2023-02-01 11:26:43 -0500

    created BasicAuthentication

--- reservationMoviesProject/settings.py
+++ reservationMoviesProject/settings.py
@@ -42,10 +42,12 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
-        "rest_framework.authentication.SessionAuthentication",
-    ),
+    "DEFAULT_AUTHENTICATION_CLASSES":
+    ['rest_framework.authentication.BasicAuthentication'],
+    'DEFAULT_PERMISSION_CLASSES':
+    ['rest_framework.permissions.IsAuthenticated'],
 }
+# AllowAny \\ IsAuthenticated \\ IsAdminUser \\ IsAuthenticatedReadOnly
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
